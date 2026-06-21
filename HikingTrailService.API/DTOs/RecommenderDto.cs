059ec429337ed0a0a696ab1909bfcce41ec354af
RubenFern <rubenferval@hotmail.com>
2026-06-21 21:10:22 +0200

    Validación de datos

--- HikingTrailService.API/DTOs/RecommenderDto.cs
+++ HikingTrailService.API/DTOs/RecommenderDto.cs
@@ -1,11 +1,18 @@
-﻿using Common.API.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+using Common.API.DTOs;
 
 namespace HikingTrailService.DTOs;
 
 public record RecommenderDto : BaseDto
 {
     public required Guid AccountCode { get; set; }
+
+    [Range(1, 25, ErrorMessage = "Kilometers must be between 1 and 25.")]
     public int Kilometers { get; init; } = 10;
+
+    [Range(-90, 90, ErrorMessage = "LocationLatitude must be between -90 and 90.")]
     public required double LocationLatitude { get; set; }
+
+    [Range(-180, 180, ErrorMessage = "LocationLongitude must be between -180 and 180.")]
     public required double LocationLongitude { get; set; }
 }
